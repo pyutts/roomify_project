acad1ccf8441c1236fd42dd854a3d19e646b0789
pyutts <wiadyana6@gmail.com>
2025-06-29 22:03:28 +0800

    Mencoba Local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
         ini_set('upload_max_filesize', '10M');
         ini_set('post_max_size', '12M');
 
-        if (env('APP_ENV') === 'debug') {
+        if (env('APP_ENV') === 'local') {
             URL::forceScheme('https');
         }
     }
